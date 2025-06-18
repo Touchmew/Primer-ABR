b3ae2fb3207ee2eda6cd3121e802f659acfd1c56
IngritE784 <60752283@continental.edu.pe>
2025-06-17 21:50:27 -0500

    Update main.cpp
    Implementacion del nodo principal

--- main.cpp
+++ main.cpp
@@ -1,2 +1,26 @@
 //hallo
+#include <iostream>
+#include <string>
+// estruct 
+
+struct Persona{
+  string  nombre;
+  String fechaNacimiento;
+  Persona* hijoIzquierdo;
+  Persona* hijoDerecho;
+  Persona(string _nombre, string _fecha)
+        : nombre(_nombre), fechaNacimiento(_fecha),
+          padre(NULL), hijoIzquierdo(NULL), hijoDerecho(NULL) {}
+};
+//Funcion crear persona
+
+Persona* crearPersona(string nombre, string fecha) {
+    return new Persona(nombre, fecha);
+}
+// Insertar, Alvaro
+// Buscar , Almir 
+//Eliminar, Daniel 
+
+//Main menu , jhul 
+
 //que se hace en esta cosa?
